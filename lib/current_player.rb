ed0556494eebd6392381aa12803b91d653e6509f
Kristyn Little <kristynlittle@frontier.com>
2021-01-25 02:53:56 +0000

    Automatically backed up by Learn

---
+++ lib/current_player.rb
@@ -0,0 +1,15 @@
+def turn_count(board)
+counter = 0
+board.each do |spaces|
+  if spaces == "X" || spaces == "O"
+    counter += 1
+  end
+end
+
+def current_player(board)
+  if turn_count(board) % 2 == 0
+    gets "X"
+  else
+    gets "O"
+  end
+end
